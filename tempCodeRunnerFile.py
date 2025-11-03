e76a6213bea3d0e5cde94a94f8354867be7c67a0
Skit-025 <codesheritage@gmail.com>
2025-11-04 00:56:50 +0530

    proper classification

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-
-        for j in range(n - i):
-            print(" ", end="")
-        
-        # Print increasing numbers
-        for j in range(1, i + 1):
-            print(j, end="")
-        
