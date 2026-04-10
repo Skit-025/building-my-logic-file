c3dbfade189a2e8e66c17f0309381c863cac1d30
Aditya prasad Barik <codesheritage@gmail.com>
2026-04-11 01:07:27 +0530

    Create inver_num.c

---
+++ strivers/logical patterns/inver_num.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int pattern(int n){
+    for(int i=0;i<5;i++){
+        for(int j=1;j<n+1-i;j++){
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+}
+int main(){
+    // int n;
+    // printf("Enter the value of the n:");
+    // scanf("%d",&n);
+    pattern(5);
+    return 1;
+}
