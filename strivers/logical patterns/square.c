c11299bab5980a65ce97a7f57a63f12fbcb8aaa3
Aditya prasad Barik <codesheritage@gmail.com>
2026-04-11 00:16:00 +0530

    Create square.c

---
+++ strivers/logical patterns/square.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//for standard input output
+int square(int n){
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        printf("*");
+    }
+    printf("\n");
+}
+}
+int main(){
+    int n;
+    printf("Enther the value of n: ");
+    scanf("%d",&n);
+    square(n);
+}
