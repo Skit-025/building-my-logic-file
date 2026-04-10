5c2c83d6fa062ad088774259242a9e99ad6ed8d0
Aditya prasad Barik <codesheritage@gmail.com>
2026-04-11 00:21:49 +0530

    Update right_triangle.c

--- strivers/logical patterns/right_triangle.c
+++ strivers/logical patterns/right_triangle.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int pattern(int n){
     for(int i=0;i<n;i++){
-
+for(int j=0;j<=i;j++){
+    printf("*");
+}printf("\n");
     }
+}
+int main(){
+    int n;
+    printf("Enter the value of n:");
+    scanf("\n%d",&n);
+    pattern(n);
+return 1;
 }
