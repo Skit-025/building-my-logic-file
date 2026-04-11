670a12896f9910915db70d46597d1af3e7d2d711
Aditya prasad Barik <codesheritage@gmail.com>
2026-04-11 22:55:55 +0530

    Create invert_triangle.c

---
+++ strivers/logical patterns/invert_triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int pattern(int n){
+    for(int i=0;i<5;i++){
+        for(int j=0;j<n-i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+int main(){
+    // int n;
+    // printf("Enter the value of the n:");
+    // scanf("%d",&n);
+    pattern(5);
+    return 1;
+}
