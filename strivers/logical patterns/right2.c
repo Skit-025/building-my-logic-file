72bdc3029e798eec6ca57093600870dee309a0b0
Aditya prasad Barik <codesheritage@gmail.com>
2026-04-12 01:31:22 +0530

    Create right2.c

---
+++ strivers/logical patterns/right2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int pattern(int n){
+for(int i=1;i<=n;i++){
+    for(int j=0;j<i;j++){
+        printf("*");
+    }
+    printf("\n");
+}
+for(int i=n-1;i>0;i--){
+    for(int j=0;j<i;j++){
+        printf("*");
+    }
+    printf("\n");
+}
+}
+int main(){
+    int n;
+    printf("Enther the value of n:");
+    scanf("%d",&n);
+    pattern(n);
+    return 1;
+}
