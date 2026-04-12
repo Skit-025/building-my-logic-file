f41059def27c07e8edee0be2017eb7175dd85db6
Aditya prasad Barik <codesheritage@gmail.com>
2026-04-13 02:47:03 +0530

    Create char.c

---
+++ strivers/logical patterns/char.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int pattern(int n){
+    for(int i=0;i<n;i++){
+        int num=65;
+        for(int j=0;j<=i;j++){
+            char ch=(char) num;
+            printf("%c",ch);
+            num++;
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int n;
+    printf("Enter the value of the n: ");
+    scanf("%d",&n);
+    pattern(n);
+return 1;
+}
