7e5376476fd8392b37161a59e34b378fccff63fa
Aditya prasad Barik <codesheritage@gmail.com>
2026-04-13 02:47:06 +0530

    Create char_triangle.c

---
+++ strivers/logical patterns/char_triangle.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int pattern(int n){
+    for(int i=1;i<=n;i++){
+        int num=65;
+        for (int j=0;j<n-i;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++){
+            char ch=(char) num;
+            printf("%c",ch);
+            num++;
+        }
+        num-=2;
+        for(int m=0;m<i-1;m++){
+            printf("%c",(char) num);
+            num--;
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int n;
+    printf("Enter the value of the n: ");
+    scanf("%d",&n);
+    pattern(n);
+return 1;
+}
