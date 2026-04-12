2cf3d28a0b157c09bf940f1633ee1773f98efdb1
Aditya prasad Barik <codesheritage@gmail.com>
2026-04-12 17:07:26 +0530

    Create num.c

---
+++ strivers/logical patterns/num.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int pattern(int n){
+    int num=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d ",num);
+            num+=1;
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int n;
+    printf("Enter the value of the n: ");
+    scanf("%d",&n);
+    pattern(n);
+return 1;
+}
