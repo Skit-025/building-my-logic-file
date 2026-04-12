b3c4429a38d715458dc8ce0c68bd14b55445e9fd
Aditya prasad Barik <codesheritage@gmail.com>
2026-04-12 16:59:59 +0530

    Create v_num.c

---
+++ strivers/logical patterns/v_num.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int pattern(int n){
+    for(int i=1;i<n;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d",j);
+        }
+        for(int s=0;s<2*(n-(i+1));s++){
+        printf(" ");
+        }
+        for(int k=i;k>0;k--){
+            printf("%d",k);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int n;
+    printf("Enter the value of the n: ");
+    scanf("%d",&n);
+    pattern(n);
+return 1;
+}
