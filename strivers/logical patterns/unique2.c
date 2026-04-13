bef05799757b81f8f329972b9305fcc859ea8495
Aditya prasad Barik <codesheritage@gmail.com>
2026-04-14 02:42:44 +0530

    Create unique2.c

---
+++ strivers/logical patterns/unique2.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void pattern(int n){
+
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<i;j++){
+            printf("*");
+        }
+        for(int s=0;s<2*(n-i);s++)
+        {
+            printf(" ");
+        }
+        for(int star=0;star<i;star++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // alternate structure
+    
+    for(int i=n;i>0;i--){
+        for(int j=0;j<i-1;j++){
+            printf("*");
+        }
+        for(int s=0;s<2*(n-i+1);s++)
+        {
+            printf(" ");
+        }
+        for(int star=0;star<i-1;star++){
+            printf("*");
+        }
+    printf("\n");
+    }
+}
+int main(){
+    int n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    pattern(n);
+    return 0;
+}
