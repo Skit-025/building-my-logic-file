e7347b47ef0d0b6e544c7ef2c459c839e3900a3a
Aditya prasad Barik <codesheritage@gmail.com>
2026-04-28 18:55:47 +0530

    Create bubblesort.c

---
+++ strivers/Sorting and Searching/Sorting/bubblesort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = n - 1; i >= 0; i--) {
+        int didSwap = 0;
+
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // swap
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                didSwap = 1;
+            }
+        }
+
+        // Optimization: stop if already sorted
+        if (!didSwap) {
+            break;
+        }
+    }
+
+    printf("After Using Bubble Sort:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Before Using Bubble Sort:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    bubbleSort(arr, n);
+
+    return 0;
+}
